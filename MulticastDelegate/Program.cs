3dc4c98fbb7866b73218efc86ff0da375933ffac
ivan-blade <ivan-blade@outlook.com>
2017-12-31 16:35:01 +0200

    end exampe

--- MulticastDelegate/Program.cs
+++ MulticastDelegate/Program.cs
@@ -6,10 +6,54 @@ using System.Threading.Tasks;
 
 namespace MulticastDelegate
 {
-    class Program
+    public delegate void StringDelegate(string aValue);
+
+    public class TestMulticastDelegate
     {
+        void PrintString(string aValue)
+        {
+            Console.WriteLine(aValue);
+        }
+
+        void PrintStringLength(string aValue)
+        {
+            Console.WriteLine("Lenght = {0}", aValue.Length);
+        }
+
+        static void PrintStringWithDate(string aValue)
+        {
+            Console.WriteLine("{0}: {1}", DateTime.Now, aValue);
+        }
+
+        static void PrintStringInvocatioList(Delegate aDelegate)
+        {
+            Console.WriteLine("(");
+            Delegate[] list = aDelegate.GetInvocationList();
+            foreach (Delegate d in list)
+            {
+                Console.WriteLine(" {0}", d.Method.Name);
+            }
+            Console.WriteLine(" )");
+        }
+
         static void Main(string[] args)
         {
+            TestMulticastDelegate tmd = new TestMulticastDelegate();
+            StringDelegate printDelegate = new StringDelegate(tmd.PrintString);
+            StringDelegate printLenghtDelegate = new StringDelegate(tmd.PrintString);
+            StringDelegate printWithDateDelegate = new StringDelegate(PrintStringWithDate);
+            PrintStringInvocatioList(printDelegate);
+            //Prints: (PrintString)
+            StringDelegate combinedDelegate = (StringDelegate)Delegate.Combine(printDelegate, printLenghtDelegate);
+            PrintStringInvocatioList(combinedDelegate);
+            //Prints: ( PrintString PrintStringLenghts )
+
+            combinedDelegate = (StringDelegate)Delegate.Combine(combinedDelegate, printWithDateDelegate);
+            PrintStringInvocatioList(combinedDelegate);
+            //Prints: ( PrintString PrintStringLenght PrintStringWithDate )
+
+            //Invoke the delegate
+            combinedDelegate("test");
         }
     }
 }
