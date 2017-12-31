fa622b9c0ae6ed1f4274e29866da89f54289f1f1
ivan-blade <ivan-blade@outlook.com>
2017-12-31 15:25:03 +0200

    Add project files.

---
+++ DotNetExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DotNetExample
+{
+    //Declaration of delegate
+    public delegate void SimpleDelegat(string aParam);
+
+    class TestDelegate
+    {
+        public static void TestFunction(string aParam)
+        {
+            Console.WriteLine("I was called by a delegate.");
+            Console.WriteLine("I got a parameter {0}.", aParam);
+        }
+
+        static void Main()
+        {
+            //Insantietion of delegate
+            SimpleDelegat simpleDelegate = new SimpleDelegat(TestFunction);
+            //Invocation of the method, pointet by a delegate
+            simpleDelegate("Any string");
+        }
+    }
+}
